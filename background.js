ee1606039ccd5252056d353b43d21774941f5964
lekaha <lekaha@gmail.com>
2014-08-07 09:38:45 +0800

    Modified background js

--- background.js
+++ background.js
@@ -90,7 +90,7 @@ function setActionPopup(enable) {
     if(!enable) {
       	chrome.browserAction.setPopup(
       		{
-      			popup: "popup.html"
+      			popup: "login.html"
       		}
       	);
     }
